b8c871d439c8925903c17e1ddb977accc2e9a722
d!4 <dia@d4ui-MacBookPro.local>
2021-01-14 12:18:17 +0900

    v-if(조건문)

---
+++ hello world/README.md
@@ -0,0 +1,7 @@
+#Learn-Vue.js - Hello Vue!
+
+Vue를 이용하여 '안녕하세요 Vue!' 라는 글을 화면에 띄운다.
+
+![img.png](img.png)
+(실행결과)
+
---
+++ v-if/app.js
@@ -0,0 +1,13 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        seen: true
+    }
+})
+
+let app01 = new Vue({
+    el: '#app-1',
+    data: {
+        seen: false
+    }
+})
