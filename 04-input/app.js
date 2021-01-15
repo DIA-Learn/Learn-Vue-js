0f3a5f16c22bf32b5ced093554be6519e40a6785
d!4 <dia@d4ui-MacBookPro.local>
2021-01-15 12:34:01 +0900

    v-on&v-model - 사용자 입력 핸들링

---
+++ 04-input/app.js
@@ -0,0 +1,18 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: '안녕하세요! Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split(' ').reverse().join(' ')
+        }
+    }
+})
+
+let app2 = new Vue({
+    el: '#app2',
+    data: {
+        message2: '안녕하세요! Vue.js!'
+    }
+})
